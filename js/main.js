353daa60977348c025fecc1387b0d0a97dd6247e
Justin Nguyen <jaykaycrystal0914@gmail.com>
2025-01-03 16:56:27 -0600

    added cataas functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 console.log("meow !!! lol");
+
+const button = document.querySelector(".cat-button");
+
+button.addEventListener("click", 
+	function () {
+		document.querySelector(".cat-image").src = "https://cataas.com/cat"
+	}
+);
